a806df3821efce6c26b8cb74d1fb740b3d725a2a
Mihails Smolins <ms2316@cam.ac.uk>
2015-11-29 16:35:37 +0000

    Task2 added

---
+++ CollectionArrayList.java
@@ -0,0 +1,58 @@
+package java.util;
+
+import java.io.IOException;
+import java.lang.IndexOutOfBoundsException;
+
+//mutable List of constant size
+public class CollectionArrayList<E> extends AbstractList<E> 
+  implements List<E>
+{
+  private int DEFAULTSIZE = 0;
+  
+  private E[] data;
+  
+  private int size;
+  
+  public CollectionArrayList() {
+    data = (E[]) new Object[DEFAULTSIZE];
+    size = DEFAULTSIZE;
+  }
+  
+  public CollectionArrayList(int newSize) {
+    data = (E[]) new Object[newSize];
+    size = newSize;
+  }
+  
+  public CollectionArrayList(Collection<? extends E> c) {
+    this(c.size());
+    Iterator<? extends E> itr = c.iterator();
+    for (int index = 0; index < size; index++) {
+      data[index] = itr.next();
+    }
+  }
+  
+  @Override
+  public E get(int index) throws IndexOutOfBoundsException {
+    if (index < size)
+      return data[index];
+    else 
+    throw new IndexOutOfBoundsException();
+  }
+  
+  @Override
+  public int size() {
+    return size;
+  }
+  
+  @Override
+  public E set(int index, E element) throws IndexOutOfBoundsException{
+      if (index < size) {
+	E tmp = data[index];
+	data[index] = element;
+	return tmp;
+      } else 
+	throw new IndexOutOfBoundsException();
+  }
+
+
+}
